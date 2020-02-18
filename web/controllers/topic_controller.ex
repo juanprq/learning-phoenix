d32486e65a39c3dbde03818d56acc3baeae89081
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-02-18 17:10:33 -0500

    Adds ability to edit topics

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -27,4 +27,26 @@ defmodule Discuss.TopicController do
       {:error, changeset} -> render(conn, "new.html", changeset: changeset)
     end
   end
+
+  def edit(conn, %{ "id" => topic_id }) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+
+    render(conn, "edit.html", changeset: changeset, topic: topic)
+  end
+
+  def update(conn, %{ "topic" => topic, "id" => topic_id }) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset, topic: old_topic)
+    end
+  end
 end
