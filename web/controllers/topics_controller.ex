42cfb86c11cba0fe587b748ceb3437569905ce85
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-02-17 22:11:20 -0500

    Adds functions to display the form of a new topic
    Also receives the information of a topic and print it

--- web/controllers/topics_controller.ex
+++ web/controllers/topics_controller.ex
@@ -1,7 +1,18 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
+  alias Discuss.Topic
+
   def new(conn, _params) do
+    changeset =
+      %Topic{}
+      |> Topic.changeset(%{})
+
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    IO.inspect(topic)
     render(conn, "new.html")
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -20,6 +20,7 @@ defmodule Discuss.Router do
     get "/test", PageController, :test
 
     get "/topics/new", TopicController, :new
+    post "/topics", TopicController, :create
   end
 
   # Other scopes may use custom stacks.
